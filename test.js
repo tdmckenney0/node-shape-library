ab8670715e939461fd984dd2f63a13427fcb979b
Tanner Mckenney <tmckenney7@outlook.com>
2018-01-04 21:10:16 -0800

    Functions.

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 var test = require('unit.js');
+var shapes = require('./lib/shapes.js');
 
 describe('Test point', function() {
-    it('Can run a test', function() { 
-        test.bool(true).isTrue();
-    });
+	it('Can run a test', function() { 
+		test.bool(true).isTrue();
+	});
 });
